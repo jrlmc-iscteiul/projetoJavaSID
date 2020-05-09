8c0ef900434f4a114fcb3e37bf3d7d74d6e3c998
aplcg-iscteiul <aplcg@iscte-iul.pt>
2020-05-09 15:45:08 +0100

    filtrtos

--- CloudToMongo/src/FiltrarMensagens.java
+++ CloudToMongo/src/FiltrarMensagens.java
@@ -53,11 +53,11 @@ public class FiltrarMensagens {
 	}
 
 	public void filtrarTemperatura(MedicoesSensores medicao) {
-		inserirNaStack(medicao, lastHumidades);
-		List<Double> limites = new ArrayList<>();
+		inserirNaStack(medicao, lastTemperaturas);
+		List<Double> limites = outliers(lastTemperaturas, lastTemperaturas.size());
 		String v = medicao.getValorMedicao();
 		double valor = Double.parseDouble(v.replace("\"", ""));
-		if (valor < limites.get(0) || valor > limites.get(1)) {
+		if(valor < limites.get(0) || valor > limites.get(1)) {
 			cloudToMongo.mongocolLixo.insert((DBObject) JSON.parse(cloudToMongo.clean(medicao.toString())));
 			System.out.println("lixo");
 		} else {
@@ -68,15 +68,17 @@ public class FiltrarMensagens {
 	}
 
 	public void filtrarHumidade(MedicoesSensores medicao) {
-//		String v = medicao.getValorMedicao();
-//		double valor = Double.parseDouble(v.replace("\"", ""));
-//		if(valor > 100 || valor < 0 || mediaLast(medicao, lastHumidades) > 0.4 || mediaLast(medicao, lastHumidades) < -0.4) {
-//			mongocolLixo.insert((DBObject) JSON.parse(clean(medicao.toString())));
-//			System.out.println("lixo");
-//		} else {
-//			mongocolHum.insert((DBObject) JSON.parse(clean(medicao.toString())));
-//			System.out.println("e");
-//		}
+		inserirNaStack(medicao, lastHumidades);
+		List<Double> limites = outliers(lastHumidades, lastHumidades.size());
+		String v = medicao.getValorMedicao();
+		double valor = Double.parseDouble(v.replace("\"", ""));
+		if(valor < limites.get(0) || valor > limites.get(1) || valor < 0 || valor > 100) {
+			cloudToMongo.mongocolLixo.insert((DBObject) JSON.parse(cloudToMongo.clean(medicao.toString())));
+			System.out.println("lixo");
+		} else {
+			cloudToMongo.mongocolHum.insert((DBObject) JSON.parse(cloudToMongo.clean(medicao.toString())));
+			System.out.println("e");
+		}
 	}
 
 	public void movimento(MedicoesSensores medicaoMovAtual) {
@@ -200,28 +202,33 @@ public class FiltrarMensagens {
 		copy.addAll(last);
 		Stack<Double> stackOrdenada = ordenarStack(copy);
 		List<Double> limites = new ArrayList<>();
-		int mid1 = size / 2;
+		int mid1 = size/2;
 		int mid2 = (size / 2) - 1;
 		double q1 = 0;
 		double q3 = 0;
-		if (size % 2 == 0) {
-			q1 = (stackOrdenada.elementAt(mid1 + mid1 / 2) + stackOrdenada.elementAt(mid1 + (mid1 / 2) + 1)) / 2;
-			q3 = (stackOrdenada.elementAt(mid2 - mid2 / 2) + stackOrdenada.elementAt(mid2 - (mid2 / 2) + 1)) / 2;
+		if (size > 8) {
+			if (size % 2 == 0) {
+				q1 = (stackOrdenada.elementAt(mid1 + mid1 / 2) + stackOrdenada.elementAt(mid1 + (mid1 / 2) + 1)) / 2;
+				q3 = (stackOrdenada.elementAt(mid2 - mid2 / 2) + stackOrdenada.elementAt(mid2 - (mid2 / 2) + 1)) / 2;
+			} else {
+				q1 = (stackOrdenada.elementAt(mid1 + mid1 / 2) / 2);
+				q3 = (stackOrdenada.elementAt(mid1 - mid1 / 2) / 2);
+			}
 		} else {
-			q1 = (stackOrdenada.elementAt(mid1 + mid1 / 2) / 2);
-			q3 = (stackOrdenada.elementAt(mid1 - mid1 / 2) / 2);
+			q1 = last.firstElement();
+			q3 = last.firstElement();
 		}
 		double aiq = q3 - q1;
-		System.out.println(stackOrdenada.elementAt(2) - stackOrdenada.elementAt(size - 2));
-		if (between(stackOrdenada.elementAt(2) - stackOrdenada.elementAt(size - 2), 0, 2)) {
-			limites.add((q1 - aiq * 12) - 3);
-			limites.add((q3 + aiq * 12) + 3);
-		} else if (between(stackOrdenada.elementAt(2) - stackOrdenada.elementAt(size - 2), 2, 5)) {
-			limites.add(q1 - aiq * 6);
-			limites.add(q3 + aiq * 6);
+		System.out.println(stackOrdenada.elementAt(2) - stackOrdenada.elementAt(size-2));
+		if(between(stackOrdenada.elementAt(2) - stackOrdenada.elementAt(size-2),0,2)) {
+			limites.add((q1-aiq*12)-3);
+			limites.add((q3+aiq*12)+3);
+		} else if(between(stackOrdenada.elementAt(2) - stackOrdenada.elementAt(size-2),2,5)) {
+			limites.add(q1-aiq*6);
+			limites.add(q3+aiq*6);
 		} else {
-			limites.add(q1 - aiq * 4);
-			limites.add(q3 + aiq * 4);
+			limites.add(q1-aiq*4);
+			limites.add(q3+aiq*4);
 		}
 		System.out.println(limites);
 		return limites;
